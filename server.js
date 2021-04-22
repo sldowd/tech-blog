9987ff5dc15b7d6400c6cb35e20bcee5346d1e7a
Sarah Dowd <sarahlynnedowd@gmail.com>
2021-04-22 10:10:03 -0400

    formatted dates and plurals

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers')
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3002;
+
 
 //create session
 const session = require('express-session');
